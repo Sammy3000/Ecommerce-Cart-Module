328d765ceecc88dbc13beb2ed58759fc1663ae20
Sammy3000 <towettsammy30@gmail.com>
2024-01-09 22:14:24 +0300

    Modify Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,17 @@
 import { useSelector } from "react-redux";
+import { CartIcon } from "../icons";
 
 const Navbar=()=>{
     return(
         <nav>
             <div className="nav-center">
-            <h3>Redux toolkit</h3>
+              <h3>Redux toolkit</h3>
+              <div className="nav-container">
+                <CartIcon/>
+                <div className="amount-container">
+                    <p className="total-amount">0</p>
+                </div>
+              </div>
             </div>
         </nav>
     )
