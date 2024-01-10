6816d24113f925a510d5e6a72cc995569128b0d9
Sammy3000 <towettsammy30@gmail.com>
2024-01-10 18:18:37 +0300

    Modify cart slice and navbar component to test the state changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { CartIcon } from "../icons";
 
 const Navbar=()=>{
+  const{amount} = useSelector(store=>store.cart)
     return(
         <nav>
             <div className="nav-center">
@@ -9,7 +10,7 @@ const Navbar=()=>{
               <div className="nav-container">
                 <CartIcon/>
                 <div className="amount-container">
-                    <p className="total-amount">0</p>
+                    <p className="total-amount">{amount}</p>
                 </div>
               </div>
             </div>
