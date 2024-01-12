70233d152519c497be4b343b9554c1f608d3471c
Sammy3000 <towettsammy30@gmail.com>
2024-01-12 10:04:34 +0300

    Select items from the store

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,6 +1,8 @@
 import CartItem from "./CartItem"
 import { useSelector } from "react-redux"
+
 const CartContainer=()=>{
+    const {cartItems,amount,total}=useSelector(store=>store.cart)
 return(
 <div>
     Cart container
