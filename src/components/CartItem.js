d87ee1e4442c0a4d595cc4108401903bc1b6da8c
Sammy3000 <towettsammy30@gmail.com>
2024-01-15 12:16:12 +0300

    Add h4 tag with title and price for ui

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -4,7 +4,10 @@
     return(
        <article className="cart-item">
         <img src={img} alt={title}/>
-
+        <div>
+            <h4>{title}</h4>
+            <h4 className="item-price">{price}</h4>
+        </div>
        </article>
     )
  }
