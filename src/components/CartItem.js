31abaebd48a2b2b5513f14c71fb977f75b58f63c
Sammy3000 <towettsammy30@gmail.com>
2024-01-15 12:29:51 +0300

    Call icons component

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -9,6 +9,10 @@
             <h4 className="item-price">{price}</h4>
             <button className="remove-btn">remove</button>
         </div>
+        <div>
+            <button className="amount-btn"><ChevronUp/></button>
+            <button className="amount-btn"><ChevronDown/></button>
+        </div>
        </article>
     )
  }
