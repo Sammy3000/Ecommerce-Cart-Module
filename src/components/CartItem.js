e9a610d48af413174ca4993db652d4172df19f41
Sammy3000 <towettsammy30@gmail.com>
2024-01-15 13:35:10 +0300

    Invoke an action

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -10,7 +10,7 @@
         <div>
             <h4>{title}</h4>
             <h4 className="item-price">{price}</h4>
-            <button className="remove-btn">remove</button>
+            <button className="remove-btn" onClick={()=>removeItem()}>remove</button>
         </div>
         <div>
             <button className="amount-btn"><ChevronUp/></button>
