b2aea2bb11e01171c21f523c54de1e13d8772c5d
Sammy3000 <towettsammy30@gmail.com>
2024-01-15 13:12:41 +0300

    Import clear cart action to component

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,5 +1,6 @@
 import CartItem from "./CartItem"
 import { useSelector,useDispatch } from "react-redux"
+import { clearCart } from "../features/cart/cartSlice"
 
 const CartContainer=()=>{
     const {cartItems,amount,total}=useSelector(store=>store.cart)
