9e41041e8ed6a9722de41c159618c93be483d4e8
Sammy3000 <towettsammy30@gmail.com>
2024-01-15 13:38:00 +0300

    Modify remove action

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -10,7 +10,7 @@
         <div>
             <h4>{title}</h4>
             <h4 className="item-price">{price}</h4>
-            <button className="remove-btn" onClick={()=>removeItem(id)}>remove</button>
+            <button className="remove-btn" onClick={()=>dispatch(removeItem(id))}>remove</button>
         </div>
         <div>
             <button className="amount-btn"><ChevronUp/></button>
