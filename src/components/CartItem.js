1838b6fd1af4d40e43c7999b6920f093866111e7
Sammy3000 <towettsammy30@gmail.com>
2024-01-18 08:28:00 +0300

    Dispatch action

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -13,7 +13,7 @@
             <button className="remove-btn" onClick={()=>dispatch(removeItem(id))}>remove</button>
         </div>
         <div>
-            <button className="amount-btn"><ChevronUp/></button>
+            <button className="amount-btn" onClick={()=>dispatch(increase({id}))}><ChevronUp/></button>
             <p className="amount">{amount}</p>
             <button className="amount-btn"><ChevronDown/></button>
         </div>
