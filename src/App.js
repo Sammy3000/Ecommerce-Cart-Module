a191b51ae794721ab03e6c141695e2f8c4583b02
Sammy3000 <towettsammy30@gmail.com>
2024-01-20 14:21:23 +0300

    Dispatch calculate totals to useefect

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import CartContainer from "./components/CartContainer";
 import { useDispatch, useSelector } from "react-redux";
 import { calculateTotals } from "./features/cart/cartSlice";
 
-
 function App() {
+  
   const dispatch = useDispatch()
+  useEffect(()=>{
+   dispatch(calculateTotals())
+  })
+
   return (
     <main>
       <Navbar/>
