fa80f55c80ccef6fa11d6c1aae9fd83af8309671
Sammy3000 <towettsammy30@gmail.com>
2024-01-21 19:09:35 +0300

    Create a buttons container

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,6 +6,7 @@ const Modal = ()=>{
         <aside className="modal-container">
             <div className="modal">
                 <h4>Remove all items from the shopping cart</h4>
+                <div className="btn-container"></div>
             </div>
 
 
