fbcb35d069153ff66287ac9e7dfb368c41da88f7
Sammy3000 <towettsammy30@gmail.com>
2024-01-23 08:26:47 +0300

    Extract isOpen state from store

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Modal from "./components/Modal";
 
 function App() {
   const {cartItems}=useSelector(store=>store.cart)
+  const {isOpen}=useSelector(store=>store.modal)
+  
   const dispatch = useDispatch()
     useEffect(()=>{
    dispatch(calculateTotals())
