a016b0dc1760b168fbb555966bb608daa1e5cc6b
Sammy3000 <towettsammy30@gmail.com>
2024-01-31 16:07:28 +0300

    Extract isLoading property from the store

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { calculateTotals,getCartItems } from "./features/cart/cartSlice";
 import Modal from "./components/Modal";
 
 function App() {
-  const {cartItems}=useSelector(store=>store.cart)
+  const {cartItems,isLoading}=useSelector(store=>store.cart)
   const {isOpen}=useSelector(store=>store.modal)
   
   const dispatch = useDispatch()
